4c8731af4ebc5dfd8e413abcf3607faea936883a
Richard Hsu <rhsu@gwu.edu>
2015-12-01 09:43:46 -0500

    Fixed routes for homepage

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -43,23 +43,12 @@
               <li class="active"><a href="#">Home</a></li>
               <li><a href="#">Add</a></li>
               <li><a href="#">Browse</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li class="dropdown-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
+              
             </ul>
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="./">Create Account</a></li>
-              <li><a href="../navbar-static-top/">Sign In</a></li>
-              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+              <li><a href="/createAccount">Create Account</a></li>
+              <li><a href="/signIn">Sign In</a></li>
+
             </ul>
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
