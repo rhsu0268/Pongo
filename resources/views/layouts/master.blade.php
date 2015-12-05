96f5d4127799c2087a2c0218aff7aec092a513d1
Richard Hsu <rhsu@gwu.edu>
2015-12-05 15:14:43 -0500

    Fixed master layout view to also include body

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -66,6 +66,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
+    {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
+    @yield('body')
 
 </body>
 </html>
