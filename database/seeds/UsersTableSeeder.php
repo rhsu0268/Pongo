96378d868afa84d7979ed42a6d01312a8888a3ed
Richard Hsu <rhsu@gwu.edu>
2015-12-06 21:58:44 -0500

    Added seeders for users (Jill and Jamal)

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // add two sample users, Jill and Jamal
+        $user = \App\User::firstOrCreate(['email' => 'jill@harvard.edu']);
+        $user->name = 'Jill';
+        $user->email = 'jill@harvard.edu';
+        $user->password = \Hash::make('helloworld');
+        $user->save();
+
+        $user = \App\User::firstOrCreate(['email' => 'jamal@harvard.edu']);
+        $user->name = 'Jamal';
+        $user->email = 'jamal@harvard.edu';
+        $user->password = \Hash::make('helloworld');
+        $user->save();
+
+    }
+}
